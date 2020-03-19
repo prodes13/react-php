f56836678ec77714d03ad2961d26fee2e0cec0ca
prodes13 <ciobotaru.florin@gmail.com>
2020-03-19 11:04:13 +0200

    backend for ping pong api

--- ping-pong-app/src/Login.js
+++ ping-pong-app/src/Login.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            errors: {}
+        }
+    }
+    handleForm = e => {
+        e.preventDefault();
+        this.props.history.push('/profile');
+    }
+    
+    handleInput = e => {
+        e.preventDefault();
+        this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
     render() { 
         return (
             <div className="flex">
                 <div className="w-1/3"></div>
                 <div className="w-1/3 mt-10 p-4 bg-white">
-                    <form className="border border-gray-400">
+                    <form className="border border-gray-400" onSubmit={this.handleForm}>
                         <div className="p-4">
                             <h1 className="text-lg border-b border-gray-300">Login</h1>
                             <div className="mt-4">
@@ -14,6 +33,8 @@ class Login extends Component {
                                 <input type="email" name="email" 
                                     placeholder="Write it here!" 
                                     className="p-2 bg-gray-200 rounded border border-gray-400 w-full"
+                                    value={this.state.email}
+                                    onChange={this.handleInput}
                                 />
                             </div>
                             <div className="mt-4">
@@ -21,6 +42,8 @@ class Login extends Component {
                                 <input type="password" name="password" 
                                     placeholder="Write it here!" 
                                     className="p-2 bg-gray-200 rounded border border-gray-400 w-full"
+                                    value={this.state.password}
+                                    onChange={this.handleInput}
                                 />
                             </div>
                             <div className="mt-4">
